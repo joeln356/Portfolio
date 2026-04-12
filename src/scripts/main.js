44448eefecf3a8e92feea16b8e583c7202b28d46
Joel <joelndala356@gmail.com>
2026-04-12 15:37:10 +0100

    Navbar: mobile-first, done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 const btnMenu = document.getElementById('btn-menu')
-
+const menu = document.querySelector('#MENU')
 
 btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('ativar')
+    menu.classList.toggle('show-menu')
+
 })
